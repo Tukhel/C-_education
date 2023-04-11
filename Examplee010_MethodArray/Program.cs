8bf3274789594e6cc8f1d67fce19b2faa2f11f94
Tukhel <andrew_melf@mail.ru>
2023-04-11 11:15:39 +1000

    Added MethodArray

---
+++ Examplee010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {2,34, 457, 43, 876, 34, 765, 3764};
+
+int n = array.Length;
+int find = 34;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
